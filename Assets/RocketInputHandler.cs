ca9630f22b24bcd7269584cd852289f064e12d4d
AJ <ajjaic@cseries.us>
2020-03-19 10:33:15 -0400

    added rocket input handler. added 'Thrust', 'RotateLeft' and 'RotateRight' virtual keys.

---
+++ Assets/RocketInputHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class RocketInputHandler : MonoBehaviour
+{
+    // messages
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetButton("Thrust"))
+        {
+            print("thrust is pressed");   
+        }
+
+        if (Input.GetButton("RotateLeft"))
+        {
+            print("rotating left");   
+        }
+        else if (Input.GetButton("RotateRight"))
+        {
+            print("rotating right");   
+        }
+    }
+}
