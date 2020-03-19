255f30903a15a28b9e50f43cd58843ecb252c4dd
AJ <ajjaic@cseries.us>
2020-03-19 11:38:30 -0400

    moved code that thrusts rocket to fixed update. now rocket can move thrust upwards when `space` is pressed

--- Assets/RocketInputHandler.cs
+++ Assets/RocketInputHandler.cs
@@ -1,22 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 
 public class RocketInputHandler : MonoBehaviour
 {
+    [SerializeField] private float forwardVelocity;
+    private Rigidbody _rigidBody;
+    private Vector3 _forceVector;
+
     // messages
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
     void Update()
     {
         ProcessInput();
     }
 
+    void FixedUpdate()
+    {
+        _rigidBody.AddRelativeForce(_forceVector);
+    }
+    
     private void ProcessInput()
     {
         if (Input.GetButton("Thrust"))
-        {
-            print("thrust is pressed");   
-        }
+            _forceVector = Vector3.up * forwardVelocity;
+        else
+            _forceVector = Vector3.zero;
 
         if (Input.GetButton("RotateLeft"))
         {
