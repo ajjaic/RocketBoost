62fcb1a69b98bba2a42ea6ec102edee00f4e36a9
AJ <ajjaic@cseries.us>
2020-03-19 13:33:24 -0400

    added ability to steer ship.

--- Assets/RocketInputHandler.cs
+++ Assets/RocketInputHandler.cs
@@ -8,7 +8,9 @@ using UnityEngine.PlayerLoop;
 public class RocketInputHandler : MonoBehaviour
 {
     [SerializeField] private float forwardVelocity;
+    [SerializeField] private float rotationVelocity;
     private Rigidbody _rigidBody;
+    private Vector3 _rotationVector;
     private Vector3 _forceVector;
 
     // messages
@@ -25,22 +27,40 @@ public class RocketInputHandler : MonoBehaviour
     void FixedUpdate()
     {
         _rigidBody.AddRelativeForce(_forceVector);
+        if (Math.Abs(_rotationVector.z) > Mathf.Epsilon)
+        {
+            var rotationDelta = Quaternion.Euler(_rotationVector);
+            _rigidBody.MoveRotation((_rigidBody.rotation * rotationDelta));
+        }
     }
     
     private void ProcessInput()
+    {
+        HandleThrust();
+        HandleRotation();
+    }
+
+    private void HandleThrust()
     {
         if (Input.GetButton("Thrust"))
             _forceVector = Vector3.up * forwardVelocity;
         else
             _forceVector = Vector3.zero;
+    }
 
+    private void HandleRotation()
+    {
         if (Input.GetButton("RotateLeft"))
         {
-            print("rotating left");   
+            _rotationVector.z = rotationVelocity;
         }
         else if (Input.GetButton("RotateRight"))
         {
-            print("rotating right");   
+            _rotationVector.z = -rotationVelocity;
+        }
+        else
+        {
+            _rotationVector.z = 0;
         }
     }
 }
