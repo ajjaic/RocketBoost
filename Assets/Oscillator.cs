4d8d4e11a834b1099908813cc3e14c47fa2dd004
AJ <ajjaic@cseries.us>
2020-03-22 09:54:13 -0400

    added oscillator obstacle that can move. uses kinematic rigidbody.

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] private Vector3 offset = new Vector3(0f, 0f, 8f);
+    private Rigidbody _rigidbody;
+    private Vector3 _startingPos;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _startingPos = _rigidbody.position;
+    }
+
+    private void FixedUpdate()
+    {
+        var cycles = Mathf.Sin(Time.time / 2f);
+        var offsetPos = offset * cycles;
+        _rigidbody.position = _startingPos + offsetPos;
+    }
+}
